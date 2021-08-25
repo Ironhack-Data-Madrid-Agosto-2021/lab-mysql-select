c8772f74c3703536c99e310372c2966c91713181
JulianTrufero <jgtrufero@gmail.com>
2021-08-26 01:23:11 +0200

    update

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -14,7 +14,7 @@ FROM titleauthor; #Misma cantidad de rows que la query anterior #
 
 /*CHALLENGE 2 */
 
-SELECT authors.au_id, au_fname, au_lname, pub_name, COUNT(authors.au_id) AS title_counts
+SELECT authors.au_id, au_fname, au_lname, title, pub_name, COUNT(pub_name) AS pub_counts
 FROM authors
 INNER JOIN titleauthor
 	ON authors.au_id = titleauthor.au_id
@@ -22,6 +22,13 @@ INNER JOIN titles
 	ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers
 	ON titles.pub_id = publishers.pub_id
-GROUP BY pub_name;
+GROUP BY authors.au_id, au_fname, au_lname, title, pub_name;
+
+
+SELECT pub_name, COUNT(*) AS pub_count
+FROM titles
+INNER JOIN publishers
+	ON titles.pub_id = publishers.pub_id
+GROUP BY ;
 
 
